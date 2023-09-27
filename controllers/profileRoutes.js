dbac87d295fa93b137723238a5c86c923bce341b
lourdes-dorvilus <lourdesdorvilus@yahoo.com>
2023-09-27 09:24:57 -0400

    added profle js to set up handlebar routes for users profile and admin

---
+++ controllers/profileRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').router();
+
+
+//http://localhost:3001/profile/admin
+router.get("/admin",(req,res) => {
+
+});
+
+
+//http://localhost:3001/profile/user    
+
+router.get("/user",(req,res) => {
+
+});
+module.exports = router;
